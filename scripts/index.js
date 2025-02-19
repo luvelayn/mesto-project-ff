71ec7dfa7a8dc6a02007221da65b3576caa76ce2
luvelayn <ktotochra9@gmail.com>
2025-02-19 18:56:37 +0300

    structure fixed, arrow functions added

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
-const cardTemplate = document.querySelector('#card-template').content;
 const cardsList = document.querySelector('.places__list');
 
-function createCard(cardData, deleteCardFunc) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+const getTemplate = () => {
+  return document
+    .querySelector('#card-template')
+    .content.querySelector('.card')
+    .cloneNode(true);
+}
+
+const createCard = (cardData, onDeleteCard) => {
+  const cardElement = getTemplate();
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
   const deleteButton = cardElement.querySelector('.card__delete-button');
@@ -11,14 +17,13 @@ function createCard(cardData, deleteCardFunc) {
   cardImage.alt = cardData.name;
   cardTitle.textContent = cardData.name;
 
-  deleteButton.addEventListener('click', (event) => deleteCardFunc(event));
+  deleteButton.addEventListener('click', () => onDeleteCard(cardElement));
 
   return cardElement;
 }
 
-function deleteCard(event) {
-  const card = event.target.closest('.card');
-  card.remove();
+const deleteCard = (cardElement) => {
+  cardElement.remove();
 }
 
 initialCards.forEach(element => {
